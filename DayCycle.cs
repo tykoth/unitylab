c5ba4251639d2f2781b8f6bcc51c7bace6a89350
Thiago Bocchile <tykoth@gmail.com>
2016-08-15 11:05:58 -0300

    Testing directional light cycle and image url to texture

---
+++ DayCycle.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Rotaciona um directional light baseado em tempo.
+public class DayCycle : MonoBehaviour {
+
+	// Objeto de luz. 
+	public Light directionalLight;
+	public float startingRotation = 0.0f; // <- temp
+	public float speed = 0.1f;
+	
+	private Vector3 lightRotation;
+	// Use this for initialization
+	void Start () {
+		lightRotation = new Vector3(0,0,0);
+		lightRotation.x = startingRotation;
+	}
+	
+	// Update is called once per frame
+	void aaaaa () {
+	
+	}
+	
+	void FixedUpdate(){
+		lightRotation.x+=((1 * Time.timeScale) * speed);
+		directionalLight.transform.rotation = Quaternion.Euler(lightRotation);
+	}
+}
---
+++ UrlToTexture.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class UrlToTexture : MonoBehaviour {
+
+	public string url;
+	
+	// Use this for initialization
+	void Start () {
+		if(url!="")	StartCoroutine(Download(url));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+	
+	public IEnumerator Download (string url) {
+		var www = new WWW (url);
+		yield return www;
+		GetComponent<Renderer>().material.mainTexture = www.texture;				
+	}	
+}
